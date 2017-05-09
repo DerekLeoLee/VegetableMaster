f884f37684bbf52d7f4172ac6a64819017776461
BlackTeaToast <p12355668@gmail.com>
2017-05-09 20:08:40 +0800

    add new activities

---
+++ app/src/main/java/tw/edu/fcu/vegetablemaster/HistoryVeg.java
@@ -0,0 +1,13 @@
+package tw.edu.fcu.vegetablemaster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HistoryVeg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history_veg);
+    }
+}
--- app/src/main/java/tw/edu/fcu/vegetablemaster/homepage.java
+++ app/src/main/java/tw/edu/fcu/vegetablemaster/homepage.java
@@ -1,13 +1,17 @@
 package tw.edu.fcu.vegetablemaster;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class homepage extends AppCompatActivity {
+
+    Button compareBtn, forecastBtn, historyBtn, suggestBtn, recipeBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +19,57 @@ public class homepage extends AppCompatActivity {
         setContentView(R.layout.activity_homepage);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        compareBtn = (Button)findViewById(R.id.compareveg);
+        forecastBtn =(Button)findViewById(R.id.forecastveg);
+        historyBtn = (Button)findViewById(R.id.historyveg);
+        suggestBtn = (Button)findViewById(R.id.suggestveg) ;
+        recipeBtn = (Button)findViewById(R.id.recipe);
+
+        compareBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(homepage.this, CompareVeg.class);
+                startActivity(intent);
+            }
+        });
+
+        forecastBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(homepage.this, ForecastVeg.class);
+                startActivity(intent);
+            }
+        });
+
+        historyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(homepage.this, HistoryVeg.class);
+                startActivity(intent);
+            }
+        });
+
+        suggestBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(homepage.this, SuggestVeg.class);
+                startActivity(intent);
+            }
+        });
+
+        recipeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(homepage.this, Recipe.class);
+                startActivity(intent);
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
