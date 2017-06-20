d9deee3d7bdff6b85f77155466d17c1af7f98b08
Jian-Hong Liao <p12355668@gmail.com>
2017-06-20 17:24:03 +0800

    finished suggest

--- app/src/main/java/tw/edu/fcu/vegetablemaster/SuggestVeg.java
+++ app/src/main/java/tw/edu/fcu/vegetablemaster/SuggestVeg.java
@@ -3,6 +3,9 @@ package tw.edu.fcu.vegetablemaster;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,7 +15,10 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
 import tw.edu.fcu.vegetablemaster.firebase.FirebaseFunction;
+import tw.edu.fcu.vegetablemaster.firebase.Vegetable;
 
 public class SuggestVeg extends AppCompatActivity {
 
@@ -25,6 +31,8 @@ public class SuggestVeg extends AppCompatActivity {
     private ImageView topImage2;
     private ImageView topImage3;
 
+    private ArrayList<Vegetable> todayVeg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,45 +42,72 @@ public class SuggestVeg extends AppCompatActivity {
         sugTop2 = (TextView) findViewById(R.id.top3_2);
         sugTop3 = (TextView) findViewById(R.id.top3_3);
 
-        topImage1 = (ImageView)findViewById(R.id.topImage01);
-        topImage2 = (ImageView)findViewById(R.id.topImage02);
-        topImage3 = (ImageView)findViewById(R.id.topImage03);
+        topImage1 = (ImageView) findViewById(R.id.topImage01);
+        topImage2 = (ImageView) findViewById(R.id.topImage02);
+        topImage3 = (ImageView) findViewById(R.id.topImage03);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("tops");
-        myRef.addValueEventListener(new ValueEventListener() {
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        DatabaseReference todayRef = database.getReference("vegetables/today");
+        todayRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
-                String top1 = dataSnapshot.child("top1").getValue(String.class);
-                String top2 = dataSnapshot.child("top2").getValue(String.class);
-                String top3 = dataSnapshot.child("top3").getValue(String.class);
-                //String[] tops = FirebaseFunction.getTops();
+                ArrayList<Vegetable> list = new ArrayList<Vegetable>();
+                for (DataSnapshot vegSnapshot : dataSnapshot.getChildren()) {
+                    list.add(vegSnapshot.getValue(Vegetable.class));
+                }
+                todayVeg = list;
 
-                sugTop1.setText("1. "+top1+" NT$50"+"/台斤");
-                sugTop2.setText("2. "+top2+" NT$50"+"/台斤");
-                sugTop3.setText("3. "+top3+" NT$50"+"/台斤");
+                DatabaseReference myRef = database.getReference("tops");
+                myRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // This method is called once with the initial value and again
+                        // whenever data at this location is updated.
+                        String top1 = dataSnapshot.child("top1").getValue(String.class);
+                        String top2 = dataSnapshot.child("top2").getValue(String.class);
+                        String top3 = dataSnapshot.child("top3").getValue(String.class);
+                        //String[] tops = FirebaseFunction.getTops();
 
-                topImage1.setImageResource(getImageId(top1));
-                topImage2.setImageResource(getImageId(top2));
-                topImage3.setImageResource(getImageId(top3));
+                        for(Vegetable veg : todayVeg) {
+                            if(veg.name.equals(top1))
+                                sugTop1.setText("1. " + top1 + " NT$" + veg.avg_price + "/台斤");
+                            if(veg.name.equals(top2))
+                                sugTop2.setText("2. " + top2 + " NT$" + veg.avg_price + "/台斤");
+                            if(veg.name.equals(top3))
+                                sugTop3.setText("3. " + top3 + " NT$" + veg.avg_price +  "/台斤");
+                        }
+
+                        topImage1.setImageResource(getImageId(top1));
+                        topImage2.setImageResource(getImageId(top2));
+                        topImage3.setImageResource(getImageId(top3));
 
 
-                Log.d(TAG, "Value is: " + top1 + top2 + top3);
+                        Log.d(TAG, "Value is: " + top1 + top2 + top3);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+                        // Failed to read value
+                        Log.w(TAG, "Failed to read value.", error.toException());
+                    }
+                });
             }
 
             @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
+            public void onCancelled(DatabaseError databaseError) {
+
             }
         });
+
+
     }
 
-    private int getImageId(String name){
+    private int getImageId(String name) {
 
-        switch (name){
+        switch (name) {
 
             case "芹菜":
                 return R.drawable.celery;
@@ -89,7 +124,7 @@ public class SuggestVeg extends AppCompatActivity {
             case "空心菜":
                 return R.drawable.spinach_1;
             case "菠菜":
-                return  R.drawable.spinach_2;
+                return R.drawable.spinach_2;
             case "花椰菜":
                 return R.drawable.cauliflower;
             case "青花椰菜":
